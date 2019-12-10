cefb0218bc8b3b43e24b94ec2445ff979b3f8fa0
TJ Cheetham <cheetham.tj@gmail.com>
2019-12-09 23:45:53 -0700

    server.js, config.json and file tree

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# workoutTracker
+# Workout Tracker
+
+### Description
+Consumers will reach their fitness goals faster when they can easily follow their workout progress. I created a Workout Tracker where users are able to view, create and track their daily workouts.
+
+
+### Technologies-Libraries
+Mongodb/Mongoose - Handlebars - Express - JavaScript - [JQuery](https://code.jquery.com/jquery-3.3.1.slim.min.js) - [Bootstrap](https://getbootstrap.com/) - CSS - HTML
+
+### Screenshots
+
+![Screenshot1](assets/images/2019-10-24(1).png)
+
+![Screenshot2](assets/images/2019-10-24(2).png)
+
+### Link
+Check it out!
+https://github.com/Tjcheetham/workoutTracker/.
---
+++ server.js
@@ -0,0 +1,34 @@
+// *** Dependencies
+// =============================================================
+var express = require("express");
+const mongojs = require("mongojs");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+const databaseUrl = "notetaker";
+const collections = ["notes"];
+
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
+//---------------------------------------------------------
+
+
+app.listen(3000, () => {
+    console.log("App running on port 3000!");
+  });
